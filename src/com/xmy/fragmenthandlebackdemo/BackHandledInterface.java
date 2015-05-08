2976968eca21e85d8900edb11d677466cbfe9e0b
Bo <JasonMrazW@users.noreply.github.com>
2015-05-08 19:41:03 +0800

    feat():添加出队接口popBackSelectedFragment

--- src/com/xmy/fragmenthandlebackdemo/BackHandledInterface.java
+++ src/com/xmy/fragmenthandlebackdemo/BackHandledInterface.java
@@ -3,4 +3,6 @@ package com.xmy.fragmenthandlebackdemo;
 public interface BackHandledInterface {
 
 	public abstract void setSelectedFragment(BackHandledFragment selectedFragment);
+	
+	public void popBackSelectedFragment(BackHandledFragment selectedFragment);
 }
